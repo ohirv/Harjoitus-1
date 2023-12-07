6b199d04193c26e2d983a6589fcfc105950879cb
ohirv <98732293+ohirv@users.noreply.github.com>
2023-12-07 15:49:04 +0200

    Console.WriteLine Muutos + MultilineKissa

--- Harjoitus 1/Program.cs
+++ Harjoitus 1/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Heippa Olga! :)");
+Console.WriteLine("Moikka Jenna! ");
+Console.WriteLine(@" /\_/\
+( o.o )
+ > ^ < ");
